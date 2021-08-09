0425f5bed5191b06d5a07fcfc4f0df81517ff3ca
Hasan <2192091007@uttarauniversity.edu.bd>
2021-08-09 17:05:46 +0600

    All File Included

--- Script.js
+++ Script.js
@@ -1,1 +1,12 @@
-
+let panels=document.querySelectorAll('.panel');
+panels.forEach(panel=>{
+    panel.addEventListener('click',function(){
+        removeClass();
+        panel.classList.add('active');
+    })
+})
+function removeClass(){
+    panels.forEach(panel=>{
+        panel.classList.remove('active');
+    })
+}
